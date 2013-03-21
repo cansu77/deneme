634121d277f32fdfc9de54d6b2790d47213b7b45
Cansu ILIK <cansu-77@hotmail.com>
2013-03-21 04:51:23 +0200

    değişiklik yapıldı.

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 
 int main()
 {
+    int sayi;
     printf("Hello world!\n");
     printf("Merhaba canim\n");
     printf("heycorçverseneborç");
+    printf("sayi söyle bana\n");
+    scanf("%d",&sayi);
     return 0;
 }
